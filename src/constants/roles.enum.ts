1315f3b93d591e025fa4c4c22a121e3b909097e7
Werter Pires <werterpires23@hotmail.com>
2025-09-03 23:34:17 -0300

    Refactor user roles descriptions for clarity and consistency

--- src/constants/roles.enum.ts
+++ src/constants/roles.enum.ts
@@ -10,37 +10,34 @@ export enum userRoleEnum {
 
 export const userRoles: userRole[] = [
   {
-    name: userRoleEnum.WATCHER,
-    description: 'Pode ver a coisa e seus filhos.'
+    name: userRoleEnum.CONTRIBUTOR,
+    description: 'Não pode nada'
   },
   {
-    name: userRoleEnum.REVIEWER,
-    description:
-      'Pode ver a coisa e seus filhos, aprovar ou desaprovar a coisa.'
+    name: userRoleEnum.WATCHER,
+    description: 'Pode ver'
   },
   {
     name: userRoleEnum.EXECUTOR,
-    description:
-      'Pode ver a coisa e seus filhos, editar os filhos, criar e deletar filhos.'
+    description: 'Pode ver e interagir'
+  },
+  {
+    name: userRoleEnum.REVIEWER,
+    description: 'Pode ver, interagir e aprovar'
   },
   {
     name: userRoleEnum.EDITOR,
-    description:
-      'Pode ver a coisa e seus filhos, editar a coisa e editar os filhos.'
-  },
-  {
-    name: userRoleEnum.CONTRIBUTOR,
-    description:
-      'Pode ver a coisa e seus filhos, criar filhos, mas não deletar.'
+    description: 'Pode ver, interagir, aprovar e adicionar filhos'
   },
   {
     name: userRoleEnum.LEADER,
     description:
-      'Pode ver a coisa e seus filhos, aprovar ou desaprovar, editar, adicionar ou remover membros, criar e deletar filhos.'
+      'Pode Ver, interagir, aprovar, adicionar filos e adicionar membros.'
   },
   {
     name: userRoleEnum.OWNER,
-    description: 'Pode fazer tudo, incluindo transferir a propriedade da coisa.'
+    description:
+      'Pode Ver, interagir, aprovar, adicionar filos, adicionar membros, editar e deletar.'
   }
 ]
 
